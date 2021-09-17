6658e9dabaff875715c9020d78b4718d0ff32b51
Rubi Yadav <73836805+rubiyadav18@users.noreply.github.com>
2021-09-17 16:35:47 +0530

    Add files via upload

---
+++ for_in.js
@@ -0,0 +1,13 @@
+var name=["rubi","sana","aaaa","ssss"]
+num=[1,2,3,4,5,6,7,]
+// dict1={"a":12,"b":33}
+for (i in num){
+    // console.log(name[i])
+    console.log(i)
+}
+
+
+
+
+// "for in it is working both object and arry"
+
