b64d1d99647f5baeca4b5bc8f6f7b0fe8f717bb1
Rubi Yadav <73836805+rubiyadav18@users.noreply.github.com>
2021-09-20 23:23:44 +0530

    Add files via upload

---
+++ paldirom.js
@@ -0,0 +1,15 @@
+const k=require("readline-sync");
+var name=k.question("enter name: ")
+const store=name;
+var string=""
+for (let i=name.length-1;i>=0;i--) {
+   string=string+name[i]
+}
+if (store===string) {
+   console.log("its palindrome string")
+}
+else {
+   console.log("it's not a palindrome string")
+}
+
+
