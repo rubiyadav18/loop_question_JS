8c3d65a6f95b0a2e80e91868d39e8663100edfd7
Rubi Yadav <73836805+rubiyadav18@users.noreply.github.com>
2021-09-20 23:25:31 +0530

    Add files via upload

---
+++ star_paten.js
@@ -0,0 +1,14 @@
+var i=3
+while (i>0){
+    var j=1
+    while (j<=4){
+        // Process.stdout.write("$"+"")
+        process.stdout.write("*"+ "")
+
+        j++;
+
+    }
+    console.log()
+    i--;
+
+}
